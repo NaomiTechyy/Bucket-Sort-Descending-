0b18a6f04beb6d5494cd5dc6856b2e17b43b6f83
Naomi Nganga <naominganga917@gmail.com>
2026-04-11 14:16:00 +0300

    Overwrite original array in descending order

--- bucketsort_desc.cpp
+++ bucketsort_desc.cpp
@@ -30,6 +30,14 @@ int main() {
     vector<int> buckets(max_val + 1, 0);
     for (int i = 0; i < n; i++) {
         buckets[arr[i]]++;
-}   
+}
+//  Overwrite original array in descending order
+    int index = 0;
+    for (int i = max_val; i >= 0; i--) {
+        while (buckets[i] > 0) {
+            arr[index++] = i;
+            buckets[i]--;
+        }
+    }   
  return 0;
 }
